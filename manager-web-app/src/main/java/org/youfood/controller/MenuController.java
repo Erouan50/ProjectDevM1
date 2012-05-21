45cfdc0d4f2c288f4f16f98c97fe2110b96f6f7d
Antoine ROUAZE <antoine.rouaze@gmail.com>
2012-05-21 02:00:42 +0200

    Adding menu list for manager.

---
+++ manager-web-app/src/main/java/org/youfood/controller/MenuController.java
@@ -0,0 +1,7 @@
+package org.youfood.controller;
+
+/**
+ * @author Antoine ROUAZE <antoine.rouaze AT zenika.com>
+ */
+public class MenuController {
+}
---
+++ manager-web-app/src/main/java/org/youfood/controller/MenuFilterController.java
@@ -0,0 +1,12 @@
+package org.youfood.controller;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * @author Antoine ROUAZE <antoine.rouaze AT zenika.com>
+ */
+@ManagedBean
+public class MenuFilterController {
+
+    private String name
+}
