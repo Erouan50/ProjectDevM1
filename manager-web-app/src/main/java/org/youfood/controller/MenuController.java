184bca1b1109f56cd14c7f82879528bdc0b92032
Antoine ROUAZE <antoine.rouaze@gmail.com>
2012-05-21 02:01:13 +0200

    Adding menu list for manager.

--- manager-web-app/src/main/java/org/youfood/controller/MenuController.java
+++ manager-web-app/src/main/java/org/youfood/controller/MenuController.java
@@ -1,7 +1,30 @@
 package org.youfood.controller;
 
+import org.youfood.model.Menu;
+import org.youfood.services.MenuService;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author Antoine ROUAZE <antoine.rouaze AT zenika.com>
  */
+@ManagedBean
 public class MenuController {
+
+    @EJB
+    private MenuService menuService;
+    private List<Menu> menus;
+
+    public List<Menu> getMenus() {
+        if (menus == null) {
+            menus = menuService.getAllMenu();
+        }
+        Logger.getLogger(MenuController.class.getName()).log(Level.WARNING, "Menus size: " + menus.size());
+        System.out.println("toto");
+        return menus;
+    }
 }
--- manager-web-app/src/main/java/org/youfood/controller/MenuFilterController.java
+++ manager-web-app/src/main/java/org/youfood/controller/MenuFilterController.java
@@ -1,6 +1,7 @@
 package org.youfood.controller;
 
 import javax.faces.bean.ManagedBean;
+import java.util.Date;
 
 /**
  * @author Antoine ROUAZE <antoine.rouaze AT zenika.com>
@@ -8,5 +9,35 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class MenuFilterController {
 
-    private String name
+    private String name;
+    private Date startDate;
+    private Date endDate;
+
+    public String doFilter() {
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
