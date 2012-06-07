02d065faf708a474d9b4fdc9201897fe46ef9616
Blist <vianney.thurotte@gmail.com>
2012-06-07 15:22:30 +0200

    first skeleton for cooker web-app

---
+++ restaurant-web-app/src/main/webapp/bootstrap/js/home.js
@@ -0,0 +1,42 @@
+var id=0;
+
+$(document).ready(function(){
+	
+
+	$("#webService").click(function(){
+		$("#order").prepend('<input type="button" id="c'+id+'" class="button-order btn btn-large btn-primary" style="margin:5px;display:none;" value="Commande n°'+id+'"/>');
+		$("#c"+id).show('drop',{direction:"up"},1000);
+		
+		id++;
+	
+	});
+	
+	$(".button-order").live("click",function(){
+
+		$(this).effect("transfer",{to:$("#infoOrder")},500,function(){
+			
+			var message = "Info Commande : "+$(this).attr("id");
+			
+			var footer = '<div><input type="button" value="Validate the order." class="validate btn btn-success" id="'+$(this).attr("id")+'" /></div>';
+			
+			var content ='<div id="'+$(this).attr("id")+'">'+message+' <br/> '+footer+' <br/></div>';
+			
+			$("#infoOrder").html(content);
+			
+		});		
+		$(this).removeClass("btn-primary").addClass("btn-warning");
+
+		
+	});
+	
+	$(".validate").live("click",function(){
+	
+		$("#"+$(this).attr('id')).hide('drop',{direction: "down"}, 500,function(){
+			$("#"+$(this).attr('id')).remove();
+			$("#"+$(this).attr('id')).remove();
+			$("#"+$(this).attr('id')).remove();
+		});
+
+	});
+
+});
