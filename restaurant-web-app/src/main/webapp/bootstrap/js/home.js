9c843e1d5b9639cfee878b095cb30788ae991729
Blist <vianney.thurotte@gmail.com>
2012-06-13 22:01:47 +0200

    add script for rabbitMQ

--- restaurant-web-app/src/main/webapp/bootstrap/js/home.js
+++ restaurant-web-app/src/main/webapp/bootstrap/js/home.js
@@ -1,32 +1,20 @@
 var id=0;
 
 $(document).ready(function(){
-	
-
-	$("#webService").click(function(){
+	/*$("#webService").click(function(){
 		$("#order").prepend('<input type="button" id="c'+id+'" class="button-order btn btn-large btn-primary" style="margin:5px;display:none;" value="Commande n°'+id+'"/>');
 		$("#c"+id).show('drop',{direction:"up"},1000);
-		
 		id++;
-	
-	});
+	});*/
 	
 	$(".button-order").live("click",function(){
-
 		$(this).effect("transfer",{to:$("#infoOrder")},500,function(){
-			
 			var message = "Info Commande : "+$(this).attr("id");
-			
 			var footer = '<div><input type="button" value="Validate the order." class="validate btn btn-success" id="'+$(this).attr("id")+'" /></div>';
-			
 			var content ='<div id="'+$(this).attr("id")+'">'+message+' <br/> '+footer+' <br/></div>';
-			
 			$("#infoOrder").html(content);
-			
 		});		
 		$(this).removeClass("btn-primary").addClass("btn-warning");
-
-		
 	});
 	
 	$(".validate").live("click",function(){
@@ -38,5 +26,15 @@ $(document).ready(function(){
 		});
 
 	});
+
+    var id = 102;
+    $("#webService").click(function(){
+        $.get("http://localhost:8080/restaurant-web-app-0.1/menus/"+id,
+            function(data){
+                alert("Data Loaded: " +   data);
+            }
+        );
+    })
+
 
 });
